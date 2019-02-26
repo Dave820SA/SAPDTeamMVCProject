9be591a6472c7a6bf9bcde96a9569681ba2c2654
Doug <dwdavidson@live.com>
2019-02-26 14:57:40 -0600

    New Changes

--- SAPDMVCTeatProject/Controllers/HomeController.cs
+++ SAPDMVCTeatProject/Controllers/HomeController.cs
@@ -3,19 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+//using MovieBusinessLayer;
 
 namespace SAPDMVCTeatProject.Controllers
 {
     public class HomeController : Controller
     {
+        //MovieContext db = new MovieContext();
         public ActionResult Index()
         {
+
+            //string movie = (from m in db.Movies
+            //                orderby m.Price descending
+            //                select m.Title).FirstOrDefault();
+            //ViewBag.HighestPrice = movie;
             return View();
         }
 
-        public ActionResult Movie()
+        public ActionResult About()
         {
-            ViewBag.Message = "This is the list of movies in the DB.";
+            ViewBag.Message = "About Page.";
 
             return View();
         }
